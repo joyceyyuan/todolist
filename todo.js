c78ce65f47d59a9f22cbf3e1dbb0e88757ea00ab
joyceyyuan <yueyuan@hotmail.com>
2023-07-30 21:09:58 -0700

    add checkbox to each task, enable user mark completed tasks

--- todo.js
+++ todo.js
@@ -1,6 +1,7 @@
 const todoInput = document.querySelector("input");
 const addButton = document.getElementById("addButton");
 const todoList = document.getElementById("todoList");
+const completedList = document.getElementById("completedList");
 
 // Create a function to add new task to the todo list
 function addTask() {
@@ -11,7 +12,7 @@ function addTask() {
     } else {
         // Create li elements for each task
         const taskItem = document.createElement("li");
-        taskItem.innerHTML = `<span>${taskName}</span> <button class="editButton">Edit</button><button class="deleteButton">Delete</button>`;
+        taskItem.innerHTML = `<input type="checkbox"><span>${taskName}</span> <button class="editButton">Edit</button><button class="deleteButton">Delete</button>`;
 
         // Append task item to the ul element
         todoList.appendChild(taskItem);
@@ -23,7 +24,7 @@ function addTask() {
 // Add click event listener to the "addButton" element 
 addButton.addEventListener("click", addTask);
 
-// Function to remove a task from the list
+// Function to remove a task from the todo list
 function deleteTask(e) {
     const taskItem = e.target.parentElement;
     todoList.removeChild(taskItem);
@@ -59,9 +60,6 @@ function editTask(e) {
         } else {
             taskItem.replaceChild(taskSpan, editInput);
             taskSpan.textContent = newTaskName;
-            /* console.log("taskItem in after clicking save button <-", taskItem);
-            console.log("e.target in saveButton function <-", e.target);*/
-
             // Replace the save button with the edit button
             const editButton = document.createElement("button");
             editButton.textContent = "Edit";
@@ -71,11 +69,39 @@ function editTask(e) {
     });
 }
 
-// add click event listener on the <ul> element, ie.todolist for event delegation
+// Function to move a task from todo list to the Completed list
+function moveTaskToCompleted(e) {
+    const taskItem = e.target.parentElement; //e.target is the checkbox input
+    const editButton = taskItem.querySelector(".editButton");
+
+    // Remove the checkbox from the task item
+    taskItem.removeChild(e.target);
+    // Remove the edit button from the task item
+    taskItem.removeChild(editButton);
+    // Move the task item to the Completed list
+    completedList.appendChild(taskItem);
+}
+
+// Function to remove a completed task from the Completed list
+function deleteCompletedTask(e) {
+    const taskItem = e.target.parentElement;
+    completedList.removeChild(taskItem);
+}
+
+// add click event listeners on the completedList
+completedList.addEventListener("click", event => {
+    if (event.target.classList.contains("deleteButton")) {
+        deleteCompletedTask(event);
+    }
+});
+
+// add click event listeners on the <ul> element, ie.todolist for event delegation
 todoList.addEventListener("click", event => {
     if (event.target.classList.contains("deleteButton")) {
         deleteTask(event);
     } else if (event.target.classList.contains("editButton")) {
         editTask(event);
+    } else if (event.target.matches("input[type='checkbox']")) {
+        moveTaskToCompleted(event);
     }
 });
